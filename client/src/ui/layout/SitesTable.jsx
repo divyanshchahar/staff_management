01f5919e2fca4c3a275152f38c711f35f0e8a146
divyanshchahar <divyanshchahar@outlook.com>
2023-11-01 22:17:57 +0530

    updated Component

--- client/src/ui/layout/SitesTable.jsx
+++ client/src/ui/layout/SitesTable.jsx
@@ -1,56 +1,50 @@
-import { useState } from "react";
-import rawSiteData from "../../files/rawSiteData.json";
-import parseDate from "../../utils/parseDate";
-import prepareSiteData from "../../utils/prepareSiteData";
-
-function SitesTable() {
-  const stateData = prepareSiteData(rawSiteData);
-
+function SitesTable({ sitesDetails, stateFn }) {
   const handleChange = (e) => {
-    const updatedSite = siteState.map((site) => {
+    const updatedSite = sitesDetails.map((site) => {
       if (site.id == e.target.name)
         return { ...site, InstallationDate: e.target.value };
 
       return site;
     });
 
-    useSiteState(updatedSite);
+    stateFn(updatedSite);
   };
 
-  const [siteState, useSiteState] = useState(stateData);
-
   return (
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Phone</th>
-        <th>City</th>
-        <th>Electrician</th>
-        <th>Date</th>
-        <th>Type</th>
-      </tr>
-      {siteState.map((site) => {
-        return (
-          <>
-            <tr key={site.id}>
-              <td>{site.name}</td>
-              <td>{site.phone}</td>
-              <td>{site.city}</td>
-              <td>{site.AssignedElectritian}</td>
-              <td>
-                <input
-                  type="date"
-                  value={site.InstallationDate}
-                  name={site.id}
-                  onChange={handleChange}
-                />
-              </td>
-              <td>{site.grievance}</td>
-            </tr>
-          </>
-        );
-      })}
-    </table>
+    <>
+      <h1>Details</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>City</th>
+          <th>Electrician</th>
+          <th>Date</th>
+          <th>Type</th>
+        </tr>
+        {sitesDetails.map((site) => {
+          return (
+            <>
+              <tr key={site.id}>
+                <td>{site.name}</td>
+                <td>{site.phone}</td>
+                <td>{site.city}</td>
+                <td>{site.AssignedElectritian}</td>
+                <td>
+                  <input
+                    type="date"
+                    value={site.InstallationDate}
+                    name={site.id}
+                    onChange={handleChange}
+                  />
+                </td>
+                <td>{site.grievance}</td>
+              </tr>
+            </>
+          );
+        })}
+      </table>
+    </>
   );
 }
 
