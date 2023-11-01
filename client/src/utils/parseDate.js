7fb26afa883edddee7cae01c5d159e28bbba89e0
divyanshchahar <divyanshchahar@outlook.com>
2023-11-01 18:22:57 +0530

    updated utility function

--- client/src/utils/parseDate.js
+++ client/src/utils/parseDate.js
@@ -1,9 +1,19 @@
 const parseDate = (date) => {
-  const day = new Date(date).getDate();
-  const month = new Date(date).getMonth() + 1;
+  const day =
+    new Date(date).getDate() < 10
+      ? `0${new Date(date).getDate()}`
+      : `${new Date(date).getDate()}`;
+
+  const month =
+    new Date(date).getMonth() + 1 < 10
+      ? `0${new Date(date).getMonth() + 1}`
+      : `${new Date(date).getMonth() + 1}`;
+
   const year = new Date(date).getFullYear();
 
-  return `${day}-${month}-${year}`;
+  console.log(`${year}-${month}-${day}`);
+
+  return `${year}-${month}-${day}`;
 };
 
 export default parseDate;
