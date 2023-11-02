8abfe4ad6ded8ed5c49a4ebda7cb793fc786eb43
divyanshchahar <divyanshchahar@outlook.com>
2023-11-02 20:52:39 +0530

    added new button

--- client/src/ui/layout/TablesLayout.jsx
+++ client/src/ui/layout/TablesLayout.jsx
@@ -4,13 +4,32 @@ import prepareSiteData from "../../utils/prepareSiteData";
 import summarizeSiteData from "../../utils/summarizeSiteData";
 import SumamryTable from "./SumamryTable";
 import SitesTable from "./SitesTable";
+import balanceLoad from "../../utils/balanceLoad";
 
 function TablesLayout() {
   const [siteState, useSiteState] = useState(prepareSiteData(rawSiteData));
   const summarizedData = summarizeSiteData(siteState);
 
+  const handleClick = () => {
+    const allowCalculation = summarizedData.map((item) => {
+      if (item.solutionFeasible) {
+        return "yes";
+      }
+      return "no";
+    });
+
+    if (allowCalculation.includes("no")) {
+      console.log(
+        "Unable to process solution, some electricians could have more than 3 site assigned to them. please make sure that all rows in Solution column of Summary row read yes"
+      );
+      return;
+    }
+    balanceLoad(summarizedData);
+  };
+
   return (
     <>
+      <button onClick={() => handleClick()}>Calculate Solution</button>
       <SitesTable sitesDetails={siteState} stateFn={useSiteState} />
       <SumamryTable summarizedData={summarizedData} />
     </>
